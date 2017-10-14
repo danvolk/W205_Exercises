37ea90f88c3bc9c8cb78b14f6501c3048599a51b
w205 <w205@ip-172-31-90-223.ec2.internal>
2017-10-14 17:43:12 +0000

    updated load and clean datalake files

--- exercise_1/loading_and_modeling/load_data_lake.sh
+++ exercise_1/loading_and_modeling/load_data_lake.sh
@@ -24,34 +24,50 @@ unzip medicare_data.zip
 OLD_FILE="Hospital General Information.csv"
 NEW_FILE="hospitals.csv"
 tail -n +2 "$OLD_FILE" >$NEW_FILE
-hdfs dfs -put $NEW_FILE /user/w205/hospital_compare
 
 # 2 remove the first line of second file, rename and copy
 OLD_FILE="Timely and Effective Care - Hospital.csv"
 NEW_FILE="effective_care.csv"
 tail -n +2 "$OLD_FILE" >$NEW_FILE
-hdfs dfs -put $NEW_FILE /user/w205/hospital_compare
 
 # 3 remove the first line of third file, rename and copy
 OLD_FILE="Complications and Deaths - Hospital.csv"
 NEW_FILE="readmissions.csv"
 tail -n +2 "$OLD_FILE" >$NEW_FILE
-hdfs dfs -put $NEW_FILE /user/w205/hospital_compare
 
 # 4 remove the first line of fourth file, rename and copy
 OLD_FILE="Measure Dates.csv"
-NEW_FILE="Measures.csv"
+NEW_FILE="measures.csv"
 tail -n +2 "$OLD_FILE" >$NEW_FILE
-hdfs dfs -put $NEW_FILE /user/w205/hospital_compare
 
 # 5 remove the first line of fifth file, rename and copy
 OLD_FILE="hvbp_hcahps_11_10_2016.csv"
-NEW_FILE="surveys_responses.csv"
+NEW_FILE="survey_responses.csv"
 tail -n +2 "$OLD_FILE" >$NEW_FILE
-hdfs dfs -put $NEW_FILE /user/w205/hospital_compare
+
+# create our main hdfs directory
+hdfs dfs -mkdir /user/w205/hospital_compare
+
+# create hdfs directory for each file and copy each files into hdfs
+hdfs dfs -mkdir /user/w205/hospital_compare/hospitals
+hdfs dfs -put hospitals.csv /user/w205/hospital_compare/hospitals
+
+hdfs dfs -mkdir /user/w205/hospital_compare/effective_care
+hdfs dfs -put effective_care.csv /user/w205/hospital_compare/effective_care
+
+hdfs dfs -mkdir /user/w205/hospital_compare/readmissions
+hdfs dfs -put readmissions.csv /user/w205/hospital_compare/readmissions
+
+hdfs dfs -mkdir /user/w205/hospital_compare/measures
+hdfs dfs -put measures.csv /user/w205/hospital_compare/measures
+
+hdfs dfs -mkdir /user/w205/hospital_compare/survey_responses
+hdfs dfs -put survey_responses.csv /user/w205/hospital_compare/survey_responses
+
 
 # change directory back to original
 cd $MY_CWD
 
 # clean exit
 exit
+
